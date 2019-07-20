959579f37725a3e8195082f9ba8cd24e33cb4a9d
Adam Eberbach <aeberbach@mac.com>
2019-07-20 14:10:54 +1000

    chapter 4

--- trace/trace.cpp
+++ trace/trace.cpp
@@ -5,14 +5,29 @@
 #include "vec3.h"
 #include "ray.h"
 
+bool hit_sphere(const vec3& center, float radius, const ray& r);
+
 vec3 color(const ray& r) {
+
+	if (hit_sphere(vec3(0, 0, -1), 0.5, r)) {
+		return vec3(1, 0, 0);
+	}
 	vec3 unit_direction = unit_vector(r.direction());
-	float t = 0.5f * (unit_direction.y() + 1.0f);
-	return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5f, 0.7f, 1.0f);
+	float t = 0.5f * (unit_direction.y() + 1.f);
+	return (1.f - t) * vec3(1.f, 1.f, 1.f) + t * vec3(0.5f, 0.7f, 1.f);
+}
+
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0f * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = b * b - 4 * a * c;
+	return (discriminant > 0);
 }
 
 int main() {
-	int nx = 200;
+	int nx = 210;
 	int ny = 100;
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 	vec3 lower_left_corner(-2.0, -1.0, -1.0);
