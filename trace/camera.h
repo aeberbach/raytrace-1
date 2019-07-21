579ba2268d59db4b37db1b0eddd0b32676e7c074
Adam Eberbach <aeberbach@mac.com>
2019-07-21 15:32:43 +1000

    chapter 11: defocus blur

--- trace/camera.h
+++ trace/camera.h
@@ -4,10 +4,18 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+vec3 random_in_unit_disc() {
+	vec3 p;
+	do {
+		p = 2.f * vec3((rand() / (RAND_MAX + 1.f)), (rand() / (RAND_MAX + 1.f)), 0.f) - vec3(1, 1, 0);
+	} while (dot(p, p) > 1.f);
+	return p;
+}
+
 class camera {
 public:
-	camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect) {
-		vec3 u, v, w;
+	camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float aperture, float focus_dist) {
+		lens_radius = aperture / 2.f;
 		float theta = vfov * float(M_PI) / 180.f;
 		float half_height = tan(theta / 2);
 		float half_width = aspect * half_height;
@@ -15,15 +23,21 @@ public:
 		w = unit_vector(lookfrom - lookat);
 		u = unit_vector(cross(vup, w));
 		v = cross(w, u);
-		lower_left_corner = origin - half_width * u - half_height * v - w;
-		horizontal = 2 * half_width * u;
-		vertical = 2 * half_height * v;
+		lower_left_corner = origin - half_width * focus_dist * u - half_height * focus_dist * v - focus_dist * w;
+		horizontal = 2 * half_width * focus_dist * u;
+		vertical = 2 * half_height * focus_dist * v;
+
 	}
-	ray get_ray(float s, float t) { return ray(origin, lower_left_corner + s * horizontal + t * vertical - origin); }
+	ray get_ray(float s, float t) { 
+		vec3 rd = lens_radius * random_in_unit_disc();
+		vec3 offset = u * rd.x() + v * rd.y();
+		return ray(origin + offset, lower_left_corner + s * horizontal + t * vertical - origin - offset); }
 
 	vec3 origin;
 	vec3 lower_left_corner;
 	vec3 horizontal;
 	vec3 vertical;
+	vec3 u, v, w;
+	float lens_radius;
 };
 
